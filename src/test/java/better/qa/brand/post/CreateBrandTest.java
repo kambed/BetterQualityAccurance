af181a8880629644977e90141fb57895f1edd382
Radosław Bucki <236507@edu.p.lodz.pl>
2023-10-29 14:57:11 +0100

    #5 Create brand tests

--- src/test/java/better/qa/brand/post/CreateBrandTest.java
+++ src/test/java/better/qa/brand/post/CreateBrandTest.java
@@ -1,6 +1,154 @@
 package better.qa.brand.post;
 
 import better.qa.TestBase;
+import better.qa.helpers.JSONReader;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import jdk.jfr.Description;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class CreateBrandTest extends TestBase {
+    private String brandId;
+
+    @Test
+    @Description("1. Basic positive tests (happy paths)")
+    public void shouldCreateBrandWhenCorrectDataAndLoggedInAsAdmin() {
+        ValidatableResponse body = given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .body(JSONReader.getJsonString(
+                        "brand/create_brand.json",
+                        Map.of(
+                                "name", "New test brand",
+                                "slug", "new-test-brand"
+                        )
+                ))
+                .post(getUrlForEndpoint("brands"))
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body(
+                        "id", notNullValue(),
+                        "name", equalTo("New test brand"),
+                        "slug", equalTo("new-test-brand")
+                );
+        brandId = body.extract().path("id");
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .get(getUrlForEndpoint("brands/%s".formatted(brandId)))
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body(
+                        "id", equalTo(brandId),
+                        "name", equalTo("New test brand"),
+                        "slug", equalTo("new-test-brand")
+                );
+    }
+
+    @Test
+    @Description("3. Negative testing – invalid input - Trying to create a brand with already existing slug")
+    public void shouldNotCreateBrandWhenCorrectDataAndLoggedInAsAdminAndBrandAlreadyExists() {
+        ValidatableResponse body = given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .body(JSONReader.getJsonString(
+                        "brand/create_brand.json",
+                        Map.of(
+                                "name", "New test brand",
+                                "slug", "new-test-brand"
+                        )
+                ))
+                .post(getUrlForEndpoint("brands"))
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body(
+                        "id", notNullValue(),
+                        "name", equalTo("New test brand"),
+                        "slug", equalTo("new-test-brand")
+                );
+        brandId = body.extract().path("id");
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .body(JSONReader.getJsonString(
+                        "brand/create_brand.json",
+                        Map.of(
+                                "name", "New test brand",
+                                "slug", "new-test-brand"
+                        )
+                ))
+                .post(getUrlForEndpoint("brands"))
+                .then()
+                .statusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+                .body("slug[0]", equalTo("A brand already exists with this slug."));
+    }
+
+    @Test
+    @Description("3. Negative testing – invalid input - Missing required parameter name")
+    public void shouldNotCreateBrandWhenRequiredParameterNameIsMissing() {
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .body(JSONReader.getJsonString("brand/create_brand_missing_name.json"))
+                .post(getUrlForEndpoint("brands"))
+                .then()
+                .statusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+                .body("name[0]", equalTo("The name field is required."));
+    }
+
+    @Test
+    @Description("3. Negative testing – invalid input - Missing required parameter slug")
+    public void shouldNotCreateBrandWhenRequiredParameterSlugIsMissing() {
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .body(JSONReader.getJsonString("brand/create_brand_missing_slug.json"))
+                .post(getUrlForEndpoint("brands"))
+                .then()
+                .statusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY)
+                .body("slug[0]", equalTo("The slug field is required."));
+    }
+
+    @Test
+    @Description("3. Destructive testing – invalid input - XML instead of JSON")
+    public void shouldNotCreateBrandWhenCorrectDataAndLoggedInAsAdminAndWrongContentTypeInPayload() {
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .body(
+                        """
+                            <?xml version="1.0" encoding="UTF-8"?>
+                            <brand>
+                                <name>Test brand</name>
+                                <slug>test-brand</slug>
+                            </brand>
+                        """
+                )
+                .post(getUrlForEndpoint("brands"))
+                .then()
+                .statusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Authorization", adminToken)
+                .delete(getUrlForEndpoint("brands/%s".formatted(brandId)));
+    }
 }
