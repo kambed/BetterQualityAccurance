7d9ab0fbd0a36fbd397ddc1dd520a9f9850e37c5
Damiaan <ddamian2057@gmail.com>
2023-11-18 15:17:33 +0100

    Update ProductTest.java

--- src/test/java/better/qa/e2e/product/ProductTest.java
+++ src/test/java/better/qa/e2e/product/ProductTest.java
@@ -3,6 +3,7 @@ package better.qa.e2e.product;
 import better.qa.e2e.TestBase;
 import jdk.jfr.Description;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,9 +14,9 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class ProductTest extends TestBase {
 
@@ -54,24 +55,8 @@ public class ProductTest extends TestBase {
     @Test
     @Description("ID: PRODUCTS_PUT_CORRECT, Product update with correct data")
     public void editProductWithCorrectData() throws InterruptedException {
-        loginAsAdmin();
+        prepareForProductEdit("01HFHBPK5FAY43YKKMCM8JGVF3");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h1[data-test='page-title']")));
-        driver.get(WEB_URL + "#/admin/products");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("table.table")));
-        WebElement table = driver.findElement(By.cssSelector("table.table"));
-
-        WebElement tbody = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(table, By.tagName("tbody")));
-        List<WebElement> rows = tbody.findElements(By.tagName("tr"));
-        WebElement firstRow = rows.get(0);
-        List<WebElement> columns = firstRow.findElements(By.tagName("td"));
-        WebElement buttons = columns.get(4);
-        WebElement editButton = buttons.findElement(By.cssSelector("a.btn.btn-sm.btn-primary"));
-        editButton.click();
-
-        //wait until data is loaded
-        Thread.sleep(2000);
 
         WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//*[@id='name']")));
@@ -104,6 +89,89 @@ public class ProductTest extends TestBase {
         int productCount = container.findElements(By.className("card")).size();
         assertEquals(productCount, 1);
         assertTrue(container.findElement(By.className("card")).getText().contains("Updated product name"));
+    }
+
+    @Test
+    @Description("ID: PRODUCTS_PUT_INCORRECT, Product update with incorrect data")
+    public void editProductWithInCorrectData() throws InterruptedException {
+        String name = prepareForProductEdit("01HFHBPK5FAY43YKKMCM8JGVF3");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id='name']")));
+        prepareInput(nameInput);
+        checkToastMessage("/html/body/app-root/div/app-products-add-edit/div/form/div[2]/div[1]/div[2]/div[2]/div", "Name is required");
+
+        WebElement descriptionInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id='description']")));
+        descriptionInput.clear();
+        descriptionInput.sendKeys(Keys.ENTER);
+        descriptionInput.sendKeys(Keys.BACK_SPACE);
+        checkToastMessage("/html/body/app-root/div/app-products-add-edit/div/form/div[2]/div[1]/div[3]/div[2]/div", "Description is required");
+
+        WebElement stockInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id='stock']")));
+        prepareInput(stockInput);
+        WebElement isPresent = driver.findElement(By.xpath("/html/body/app-root/div/app-products-add-edit/div/form/div[2]/div[1]/div[4]/div[2]"));
+        assertTrue(isPresent.isDisplayed());
+
+        WebElement priceInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id='price']")));
+        prepareInput(priceInput);
+        checkToastMessage("/html/body/app-root/div/app-products-add-edit/div/form/div[2]/div[1]/div[5]/div[2]/div", "Price is required");
+
+        WebElement container = searchedByPhrase(name);
+        int productCount = container.findElements(By.className("card")).size();
+        assertEquals(productCount, 1);
+        assertTrue(container.findElement(By.className("card")).getText().contains(name));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private void checkToastMessage(String xPath, String message) {
+        try {
+            WebElement element = driver.findElement(By.xpath(xPath));
+            assertEquals(element.getText(), message);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    private void prepareInput(WebElement input) {
+        input.clear();
+        input.sendKeys("d");
+        input.sendKeys(Keys.BACK_SPACE);
+        input.sendKeys(Keys.ENTER);
+    }
+
+    private String prepareForProductEdit(String uuid) throws InterruptedException {
+        loginAsAdmin();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h1[data-test='page-title']")));
+        driver.get(WEB_URL + "#/admin/products");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("table.table")));
+        WebElement table = driver.findElement(By.cssSelector("table.table"));
+
+        WebElement tbody = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(table, By.tagName("tbody")));
+        List<WebElement> rows = tbody.findElements(By.tagName("tr"));
+        WebElement row = rows.stream()
+                .filter(r -> r.getText().contains(uuid))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("No such element"));
+        List<WebElement> columns = row.findElements(By.tagName("td"));
+        WebElement buttons = columns.get(4);
+        String name = columns.get(1).getText();
+        WebElement editButton = buttons.findElement(By.cssSelector("a.btn.btn-sm.btn-primary"));
+        editButton.click();
+
+        //wait until data is loaded
+        Thread.sleep(2000);
+
+        return name;
     }
 
     private void loginAsAdmin() {
@@ -139,10 +207,5 @@ public class ProductTest extends TestBase {
 
         return wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.cssSelector("div.container[data-test='search_completed']")));
-    }
-
-    @AfterMethod
-    public void tearDown() {
-//        driver.quit();
     }
 }
