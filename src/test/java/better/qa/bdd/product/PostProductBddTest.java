f1b19365fe0dea9ca3202137665463031ac3bad7
Radosław Bucki <236507@edu.p.lodz.pl>
2023-12-05 22:52:23 +0100

    Refactor tests to fill data in separate steps

--- src/test/java/better/qa/bdd/product/PostProductBddTest.java
+++ src/test/java/better/qa/bdd/product/PostProductBddTest.java
@@ -10,10 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import java.util.List;
-
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class PostProductBddTest extends BddTestBase {
 
@@ -29,9 +25,40 @@ public class PostProductBddTest extends BddTestBase {
         goToAddProductPage();
     }
 
-    @When("User fills in product details without name")
-    public void userFillsInProductDetailsWithoutName() {
-        fillAddProductDataWithoutName();
+    @When("User fills product name with {string}")
+    public void userFillsProductNameWith(String productName) {
+        fillProductData("name", productName);
+    }
+
+    @When("User fills product description with {string}")
+    public void userFillsProductDescriptionWith(String productDescription) {
+        WebElement name = driver.findElement(By.cssSelector("textarea[formcontrolname='description']"));
+        name.sendKeys(productDescription);
+    }
+
+    @When("User fills product stock with {string}")
+    public void userFillsProductStockWith(String productStock) {
+        fillProductData("stock", productStock);
+    }
+
+    @When("User fills product price with {string}")
+    public void userFillsProductPriceWith(String productPrice) {
+        fillProductData("price", productPrice);
+    }
+
+    @When("User selects product brand {string}")
+    public void userSelectsProductBrand(String brandName) {
+        selectProductData("brand_id", brandName);
+    }
+
+    @When("User selects product category {string}")
+    public void userSelectsProductCategory(String categoryName) {
+        selectProductData("category_id", categoryName);
+    }
+
+    @When("User selects product image {string}")
+    public void userSelectsProductImage(String imageName) {
+        selectProductData("product_image_id", imageName);
     }
 
     @When("User submits new product")
@@ -40,21 +67,13 @@ public class PostProductBddTest extends BddTestBase {
         submitButton.click();
     }
 
-    @When("User fills in product details with negative price")
-    public void userFillsInProductDetailsWithNegativePrice() {
-        WebElement name = driver.findElement(By.cssSelector("input[formcontrolname='name']"));
-        name.sendKeys("Better Hammer");
-        fillAddProductDataWithoutName();
-
-        WebElement price = driver.findElement(By.cssSelector("input[formcontrolname='price']"));
-        price.clear();
-        price.sendKeys("-12345");
-    }
-
     @Then("Error message {string} is displayed")
     public void errorMessageIsDisplayed(String message) {
-        //Check if error message is displayed
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'" + message + "')]")));
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//*[contains(text(),'" + message + "')]")
+                )
+        );
     }
 
     @After
@@ -77,40 +96,20 @@ public class PostProductBddTest extends BddTestBase {
         addNewProductButton.click();
     }
 
-    private void fillAddProductDataWithoutName() {
-        WebElement description = driver.findElement(By.cssSelector("textarea[formcontrolname='description']"));
-        description.sendKeys("Better hammer for better nails");
+    private void fillProductData(String field, String value) {
+        WebElement name = driver.findElement(By.cssSelector("input[formcontrolname='" + field + "']"));
+        name.sendKeys(value);
+    }
 
-        WebElement stock = driver.findElement(By.cssSelector("input[formcontrolname='stock']"));
-        stock.sendKeys("1");
-
-        WebElement price = driver.findElement(By.cssSelector("input[formcontrolname='price']"));
-        price.sendKeys("12345");
-
-        WebElement itemForRent = driver.findElement(By.cssSelector("input[formcontrolname='price']"));
-        itemForRent.click();
-
-        WebElement brand = driver.findElement(By.cssSelector("select[formcontrolname='brand_id']"));
-        Select brandSelect = new Select(brand);
-        wait.until(d -> brandSelect.getOptions().size() > 1);
-        assertEquals(brandSelect.getOptions().size(), 3);
-        assertTrue(brandSelect.getOptions().stream().map(WebElement::getText).toList().containsAll(List.of("", "Brand name 1", "Brand name 2")));
-        brandSelect.getOptions().stream().filter(option -> option.getText().equals("Brand name 1")).findFirst().orElseThrow().click();
-
-        WebElement category = driver.findElement(By.cssSelector("select[formcontrolname='category_id']"));
-        Select categorySelect = new Select(category);
-        wait.until(d -> categorySelect.getOptions().size() > 1);
-        assertEquals(categorySelect.getOptions().size(), 13);
-        assertTrue(categorySelect.getOptions().stream().map(WebElement::getText).toList().containsAll(List.of(
-                "", "Hand Tools", "Power Tools", "Other", "Hammer", "Hand Saw", "Wrench", "Screwdriver", "Pliers", "Grinder", "Sander", "Saw", "Drill")
-        ));
-        categorySelect.getOptions().stream().filter(option -> option.getText().equals("Hand Tools")).findFirst().orElseThrow().click();
-
-        WebElement image = driver.findElement(By.cssSelector("select[formcontrolname='product_image_id']"));
-        Select imageSelect = new Select(image);
-        wait.until(d -> imageSelect.getOptions().size() > 1);
-        assertEquals(imageSelect.getOptions().size(), 30);
-        assertTrue(imageSelect.getOptions().stream().map(WebElement::getText).toList().contains("Hammer"));
-        imageSelect.getOptions().stream().filter(option -> option.getText().equals("Hammer")).findFirst().orElseThrow().click();
+    private void selectProductData(String field, String value) {
+        WebElement name = driver.findElement(By.cssSelector("select[formcontrolname='" + field + "']"));
+        Select select = new Select(name);
+        wait.until(d -> select.getOptions().size() > 1);
+        select.getOptions()
+                .stream()
+                .filter(option -> option.getText().equals(value))
+                .findFirst()
+                .orElseThrow()
+                .click();
     }
 }
--- src/test/resources/features/PostProduct.feature
+++ src/test/resources/features/PostProduct.feature
@@ -1,11 +1,22 @@
 Feature: Create new product
   Scenario: Create a new product without name
     Given User is logged as admin and is on add product page
-    When User fills in product details without name
+    When User fills product description with "Better hammer for better nails"
+    When User fills product stock with "1"
+    When User fills product price with "12345"
+    When User selects product brand "Brand name 1"
+    When User selects product category "Hand Tools"
+    When User selects product image "Hammer"
     When User submits new product
     Then Error message "Name is required" is displayed
   Scenario: Create a new product with negative price
     Given User is logged as admin and is on add product page
-    When User fills in product details with negative price
+    When User fills product name with "Hammer"
+    When User fills product description with "Better hammer for better nails"
+    When User fills product stock with "1"
+    When User fills product price with "-12345"
+    When User selects product brand "Brand name 1"
+    When User selects product category "Hand Tools"
+    When User selects product image "Hammer"
     When User submits new product
     Then Error message "Price should not be negative" is displayed
